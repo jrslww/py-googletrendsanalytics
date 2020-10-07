467555519a921b7b2496853a8857f47a351d157e
jrslww <j.wierzbicki1997@gmail.com>
2020-10-07 20:46:08 +0300

    rewriting

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ top = tkinter.Tk()
 #from pytrends.request import TrendReq
 #pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1)
 
-pytrend = TrenёdReq(hl='en-US', tz=360)
+pytrend = TrendReq(hl='en-US', tz=360)
 n = int(input('Enter a number of tendencies: '))
 keywords = []
 print("Enter tendencies: ")
